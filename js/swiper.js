862a68206cfd2038c6c2f21bc4c06a6c16a7fb8a
antonyZhang <antony88883@gmail.com>
2024-02-24 20:15:04 +0800

    Add custom pagination

--- js/swiper.js
+++ js/swiper.js
@@ -8,5 +8,9 @@ const swiper = new Swiper('.swiper', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
   },
 });
